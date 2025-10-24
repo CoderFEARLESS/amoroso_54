54c3f115500767bc823ffb58f45979f30e6fac5f
Willard <edrich.amorosoop@gmail.com>
2025-10-23 23:24:26 -0400

    Midterm

--- facebook_amoroso/lib/notification.dart
+++ facebook_amoroso/lib/notification.dart
@@ -10,6 +10,130 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications'),
+        backgroundColor: Colors.blue,
+        actions: [
+          Icon(Icons.more_vert),
+          Icon(Icons.search),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Today",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ),
+
+                Container(
+                  color: Colors.lightBlue[50],
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/profile/prof5.jpg'),
+                      backgroundColor: Colors.grey[300],
+                    ),
+                    title: Text("Learn how Meta will use your info"),
+                    subtitle: Text("16h"),
+                    trailing: Icon(Icons.more_vert),
+                  ),
+                ),
+                Divider(height: 1),
+
+                Container(
+                  color: Colors.lightBlue[50],
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/profile/prof4.jpg'),
+                      backgroundColor: Colors.grey[300],
+                    ),
+                    title: Text("5 Unknown Facts recently shared 1 post."),
+                    subtitle: Text("20h"),
+                    trailing: Icon(Icons.more_vert),
+                  ),
+                ),
+                Divider(height: 1),
+
+                Container(
+                  color: Colors.lightBlue[50],
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/profile/prof1.jpg'),
+                      backgroundColor: Colors.grey[300],
+                    ),
+                    title: Text("Fandango posted a new reel."),
+                    subtitle: Text("2h"),
+                    trailing: Icon(Icons.more_vert),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Earlier",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ),
+
+                Container(
+                  color: Colors.lightBlue[50],
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/profile/prof2.jpg'),
+                      backgroundColor: Colors.grey[300],
+                    ),
+                    title: Text("9GAG posted a new reel: 'White cat licks black cat'"),
+                    subtitle: Text("1d"),
+                    trailing: Icon(Icons.more_vert),
+                  ),
+                ),
+                Divider(height: 1),
+
+                Container(
+                  color: Colors.lightBlue[50],
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/profile/prof2.jpg'),
+                      backgroundColor: Colors.grey[300],
+                    ),
+                    title: Text("All Def Music posted a new reel."),
+                    subtitle: Text("2d"),
+                    trailing: Icon(Icons.more_vert),
+                  ),
+                ),
+                Divider(height: 1),
+
+                Container(
+                  color: Colors.lightBlue[50],
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/profile/prof1.jpg'),
+                      backgroundColor: Colors.grey[300],
+                    ),
+                    title: Text("LADbible posted a new reel."),
+                    subtitle: Text("2d"),
+                    trailing: Icon(Icons.more_vert),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+                Center(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text("See previous notifications"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
